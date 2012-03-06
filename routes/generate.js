8657ed12771747a87abb7cffea89418ddde9c487
jspears <speajus@gmail.com>
2012-03-06 08:46:02 -0500

    *Allowed for super.

--- routes/generate.js
+++ routes/generate.js
@@ -2,20 +2,20 @@ var mongoose = require('mongoose');
 module.exports = function (app) {
     var jsRe = /\.js$/;
 
-    app.get('/js/views/:type/:view', function (req, res, next) {
+    app.get('/js/:super?/views/:type/:view', function (req, res, next) {
    //     res.contentType('application/javascript');
         res.render(j('generate', 'views', req.params.view), makeOptions(req));
     });
-    app.get('/js/:clazz/:type', function (req, res, next) {
+    app.get('/js/:super?/:clazz/:type', function (req, res, next) {
    //     res.contentType('application/javascript');
         res.render(j('generate', req.params.clazz+'.js'), makeOptions(req))
     });
-    app.get('/templates/:type/:view', function (req, res, next) {
+    app.get('/templates/:super?/:type/:view', function (req, res, next) {
      //   res.contentType('text/html; charset=utf-8');
        res.render(j('generate', 'templates', req.params.view), makeOptions(req))
 
     });
-    app.get('/tpl/:view', function (req, res, next) {
+    app.get('/tpl/:super?/:view', function (req, res, next) {
         //   res.contentType('text/html; charset=utf-8');
         res.render(j('generate', 'templates', req.params.view), makeOptions(req))
 
