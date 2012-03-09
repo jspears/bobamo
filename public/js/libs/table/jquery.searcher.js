3f253522754eb0529d16345a8ab7553ee042ecdb
jspears <speajus@gmail.com>
2012-03-09 15:55:40 -0500

    *small fixes due to jQuery vs jquery. Should work better.

--- public/js/libs/table/jquery.searcher.js
+++ public/js/libs/table/jquery.searcher.js
@@ -13,44 +13,65 @@
 
         this.options = $.extend({}, $.fn.searcher.defaults, options)
         var o = this.options;
-        this.$element = $(element)
-        if (o.fields)
-            if (! $.isArray(o.fields)) {
+        var $ele = this.$element = $(element)
+        if (o.fields) {
+            if (!$.isArray(o.fields)) {
                 o.fields = o.fields.split(comma);
             } else {
-                this.$element.attr('data-fields', o.fields.join(','));
+                $ele.attr('data-fields', o.fields.join(','));
             }
-        if (o.labels)
-            if (! $.isArray(o.labels)) {
+        }
+        if (o.labels) {
+            if (!$.isArray(o.labels)) {
                 o.labels = o.labels.split(comma);
             } else {
-                this.$element.attr('data-labels', o.labels.join(','));
-
+                $ele.attr('data-labels', o.labels.join(','));
             }
+        }
 
         var $pel = this.$el = $('<form class="form-search"><input type="text" class="input-medium search-query"><div class="btn-group" data-toggle="buttons-checkbox"></div></form>')
         this.$controls = $('.btn-group', $pel);
+        this.$query = $('.search-query', $pel);
         $('input', $pel).attr('placholder', this.options.placeholder);
-        var $ele = $(element);
-        $ele.delegate('input[type=checkbox]', 'click ', $.proxy(this.change, this));
+        $ele.delegate('a.btn', 'click ', $.proxy(this.change, this));
+        $ele.delegate('input[type=text]', 'blur', $.proxy(this.changeText, this))
         $ele.append(this.$el);
         this.drawLabels();
     };
 
     Searcher.prototype = {
         constructor:Searcher,
+        changeText:function (e) {
+            var v = this.$query.val();
+            if (this._val != $.trim(v)) {
+                this._val = v;
+                this.change(e);
+            }
+        },
         change:function () {
-            console.log('change');
+            if (this._val) {
+                $(this.$element).trigger({type:'search-change', query:this.activeFields()});
+                console.log('change');
+
+            }
         },
         drawLabels:function () {
             console.log('options', this.options);
             var labels = this.options.labels || [], fields = this.options.fields || [];
-            console.log('labels',labels,'fields',fields);
-            for (var i = 0, l = Math.max(labels.length, fields.length); i < l; i++) {
-                var label = (i<labels.length) ? labels[i] : fields[i];
-                var field = (i<fields.length) ? fields[i] : labels[i];
+            console.log('labels', labels, 'fields', fields);
+            for (var i = 0, f = fields.length, a = labels.length, l = Math.max(labels.length, fields.length); i < l; i++) {
+                var label = (i < a) ? labels[i] : fields[i];
+                var field = (i < f) ? fields[i] : labels[i];
                 this.$controls.append('<a class="btn  btn-mini"  data-field="' + field + '">' + label + '</a>');
             }
+        },
+        activeFields:function () {
+            var fields = {};
+            var term = this.$query.val();
+            $('.btn.active', this.$el).each(function (k, v) {
+                fields[$(v).attr('data-field')] = term;
+            });
+            return fields;
         }
     };
 
