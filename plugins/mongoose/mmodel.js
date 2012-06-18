b6f16aa3d3ce937b850e22e39a443122c8bc0223
jspears <speajus@gmail.com>
2012-06-18 14:33:38 -0400

    *Allow for saving of existing images using refs.

--- plugins/mongoose/mmodel.js
+++ plugins/mongoose/mmodel.js
@@ -1,5 +1,4 @@
-var MField = require('./mfield'), util = require('../../lib/util'), _u = require('underscore'), inflection = require('../../lib/inflection');
-var findRe = /^find/;
+var MField = require('./mfield'), util=require('../../lib/util'), _u = require('underscore');
 module.exports = function MModel(m, manager) {
     this.__defineGetter__('modelName', function () {
         return m.modelName;
@@ -15,19 +14,7 @@ module.exports = function MModel(m, manager) {
     this.__defineGetter__('description', function () {
         return m.description;
     });
-    this.__defineGetter__('finders', function () {
-        var finders = [];
-        _u(m.schema.statics).each(function (v, k) {
-            if ( (findRe.test(k) && v.length == 0 ) || v.display) {
-                finders.push({
-                    title:inflection.titleize(inflection.humanize(k)),
-                    name:k,
-                    display:v.display
-                });
-            }
-        });
-        return finders;
-    });
+
     var display = util.depth(m, ['schema', 'options', 'display'], {});
     this.fields = display.fields;
     this.list_fields = display.list_fields;
@@ -35,14 +22,14 @@ module.exports = function MModel(m, manager) {
     this.labelAttr = display.labelAttr;
     this.fieldsets = display.fieldsets;
     this.all_fields = [];
-    this.__defineGetter__('list_fields', function () {
+    this.__defineGetter__('list_fields', function(){
         if (display.list_fields)
             return display.list_fields;
 
     });
     this.__defineGetter__('paths', function () {
         var ret = {};
-        _u.each(m.schema.tree, function (v, k) {
+        _u.each(m.schema.tree, function (v,k) {
             ret[k] = new MField(k, manager.pluginFor(k, v, m, this));
         }, this);
 
