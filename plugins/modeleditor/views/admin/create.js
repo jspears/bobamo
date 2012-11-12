5c3688adf00ca7428648afc869e6bde71491cbf8
Al Tsang <agilevpe@gmail.com>
2012-11-12 15:20:35 -0800

    messing around with different form options

--- plugins/modeleditor/views/admin/create.js
+++ plugins/modeleditor/views/admin/create.js
@@ -1,92 +1,57 @@
 define([
-    'jquery',
     'underscore',
     'Backbone',
-    'libs/jsonform/jsonform',
-    'libs/jsv/jsv',
-    'text!${pluginUrl}/templates/admin/create.html'
-], function ($, _, Backbone, jsonForm, jsv, createTemplate) {
+    'Backbone.Form',
+    'text!${pluginUrl}/templates/admin/create.html',
+    'jquery-ui',
+    'libs/backbone-forms/src/jquery-ui-editors',
+    'libs/editors/multi-editor'
+], function (_, Backbone, Backbone.Form, jsonForm, jsv, createTemplate) {
     "use strict";
     console.log('create template');
 
-    var JSONFormView = Backbone.View.extend({
-        tagName:"form",
-        initialize: function() {
-            $('form').jsonForm(
-                {
-                    "schema":{
-                        "fields":{
-                            "type":"array",
-                            "items":{
-                                "type":"object",
-                                "title":"Properties",
-                                "properties":{
-                                    "name":{
-                                        "type":"string",
-                                        "title":"Name"
-                                    },
-                                    "dataType":{
-                                        "type":"string",
-                                        "title":"Data Type",
-                                        "enum":[
-                                            "string",
-                                            "number",
-                                            "boolean",
-                                            "array",
-                                            "object",
-                                            "nested",
-                                            "reference"
-                                        ]
-                                    },
-                                    "option":{
-                                        "type":"string",
-                                        "title":"Option"
-                                    },
-                                    "required":{
-                                        "type":"boolean",
-                                        "title":"Required"
-                                    }
-                                }
-                            }
-                        }
-                    },
-                    "form":[
-                        {
-                            "type":"tabarray",
-                            "items":{
-                                "type":"section",
-                                "legend":"Field",
-                                "items":[
-                                    {
-                                        "key":"fields[]",
-                                        "title":"Properties of Field"
-                                    },
-                                    {
-                                        "type": "button",
-                                        "title": "Click me",
-                                        "onClick": function (evt) {
-                                            evt.preventDefault();
-                                            alert('Thank you!');
-                                        }
-                                    }
-                                ]
-                            }
-                        }
-                    ]
-                }
-            );
-        },
-        render:function() {
-            var $el = $(this.el);
-            return this;
+    var User = Backbone.Model.extend({
+        schema: {
+            title:      { type: 'Select', options: ['', 'Mr', 'Mrs', 'Ms'] },
+            name:       'Text',
+            email:      { validators: ['required', 'email'] },
+            birthday:   'Date',
+            password:   'Password',
+            notes:      { type: 'List' },
+            weapons:    { type: 'List', itemType: 'Object', subSchema: {
+                name: { validators: ['required'] },
+                number: 'Number'
+            }}
         }
     });
+
+    var user = new User({
+        title: 'Mr',
+        name: 'Sterling Archer',
+        email: 'sterling@isis.com',
+        birthday: new Date(1978, 6, 12),
+        password: 'dangerzone',
+        notes: [
+            'Buy new turtleneck',
+            'Call Woodhouse',
+            'Buy booze'
+        ],
+        weapons: [
+            { name: 'Uzi', number: 2 },
+            { name: 'Shotgun', number: 1 }
+        ]
+    });
+
+    var form = new Backbone.Form({
+        model: user
+    }).render();
+
 
     var CreateModelView =  Backbone.View.extend({
         tagName:'div',
         classNames:['span11'],
         template:_.template(createTemplate),
-        jsonFormView:JSONFormView,
+        modelForm:form,
         initialize: function(){
             this.render();
         },
