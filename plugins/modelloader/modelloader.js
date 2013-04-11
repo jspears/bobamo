e3122752a65c603b80b2fae5572deb889b6eca0c
Justin Spears <speajus@gmail.com>
2013-04-11 10:46:46 -0400

    *fixed modelloader

--- plugins/modelloader/modelloader.js
+++ plugins/modelloader/modelloader.js
@@ -4,8 +4,9 @@ var bobamo = require('../../index'), path = require('path'), PluginApi = bobamo.
 var ModelLoaderPlugin = function () {
     PluginApi.apply(this, arguments);
     this.conf = {
-        modelDir: process.cwd() + '/model/'
+        modelDir: path.join(process.cwd(), 'model')
     }
+    console.log('modelloader using dir ['+this.conf.modelDir+']');
 }
 u.inherits(ModelLoaderPlugin, PluginApi);
 ModelLoaderPlugin.prototype.admin = function () {
@@ -23,8 +24,13 @@ ModelLoaderPlugin.prototype.admin = function () {
     });
 };
 ModelLoaderPlugin.prototype.updateSchema = function (modelName, model) {
+    console.log('modelloader updateSchema', modelName, this.conf.modelDir);
+    var modelDir = this.conf.modelDir || path.join(process.cwd(), 'model');
+    var modelNamePath = path.join(modelDir, modelName);
+    model = model || this.pluginManager.appModel.modelPaths[modelName];
+    console.log('modelDir', modelNamePath);
     try {
-        var m = require(path.join(this.conf.modelDir, modelName))(this.pluginManager.exec('mongoose', 'schemaFor', model), this);
+        var m = require(modelNamePath)(this.pluginManager.exec('mongoose', 'schemaFor', model), this);
         if (m) {
             try {
                 this.options.mongoose.model(modelName, m);
@@ -36,7 +42,7 @@ ModelLoaderPlugin.prototype.updateSchema = function (modelName, model) {
             console.log('could not load', modelName, 'Did not return');
         }
     } catch (e) {
-        //console.error('could not load ', modelName, e);
+        console.error('could not load ', modelName, e);
     }
 }
 
