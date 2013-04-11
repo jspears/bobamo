2ee29371c659744229752de6a90e9771c3ea238e
Justin Spears <speajus@gmail.com>
2013-04-11 11:48:50 -0400

    *fixed document generation.

--- plugins/renderer/views/Collection.js
+++ plugins/renderer/views/Collection.js
@@ -12,17 +12,22 @@ define(['underscore', 'jquery'], function (_, $) {
         })
         return function (value) {
             var $el = this.$el;
-            function update(v){
+
+            function update(v) {
                 $el.html(v && v.toString() || value);
             }
-            var suc = {
-                success:update
-            }
+
             if (value) {
                 $.when(Collection).then(function (c) {
                     var v = c.get(value);
                     if (v) update(v);
-                    new c.model({id: value}).fetch(suc);
+                    new c.model({id: value}).fetch({
+                        success: function (v) {
+                         //   if (!c.get(value))
+                                c.add(v);
+                            update(v);
+                        }
+                    });
 
                 });
             }
